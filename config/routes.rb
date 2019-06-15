16dd685c5c59325eee1f1b40547aa76feec1847a
Takahiro Masui <takahiro.lux@gmail.com>
2019-06-15 12:30:18 +0900

    refactoring function definition

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'static/top'
   post 'static/ajax_update', to: 'static#ajax_update'
   get 'static/ajax_update2/:param', to: 'static#ajax_update2'
+  post 'static/ajax_update2/', to: 'static#ajax_update2'
 end
